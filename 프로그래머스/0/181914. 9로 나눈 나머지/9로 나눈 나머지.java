cd8e2dc3068c5e7d628c6e7fbb1ea64cedc84ff7
RiaLeee <gyurim26@gmail.com>
2024-11-28 00:36:53 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 5.91 ms, Memory: 90.1 MB -BaekjoonHub

--- 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
+++ 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
@@ -1,13 +1,13 @@
 class Solution {
     public int solution(String number) {
-        int sum = 0;
-
-        // number의 각 자릿수를 더함
-        for (char ch : number.toCharArray()) {
-            sum += ch - '0'; // char를 int로 변환하여 더함
+        
+        char[] ch = number.toCharArray();
+        int sum=0;
+        
+        for(char c: ch) {
+            sum += c - '0';
         }
-
-        // 합을 9로 나눈 나머지 반환
-        return sum % 9;
+      
+        return sum%9;
     }
 }
