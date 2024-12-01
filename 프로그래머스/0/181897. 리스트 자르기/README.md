d108bac96f0e5b5e64d97baacbe00930c2621a2c
RiaLeee <gyurim26@gmail.com>
2024-12-01 22:27:45 +0900

    [level 0] Title: 리스트 자르기, Time: 2.96 ms, Memory: 90 MB -BaekjoonHub

--- 프로그래머스/0/181897. 리스트 자르기/README.md
+++ 프로그래머스/0/181897. 리스트 자르기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 리스트 자르기 - 181897 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181897?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181897?language=java) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.00 ms
+메모리: 90 MB, 시간: 2.96 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 01일 22:15:02
+2024년 12월 01일 22:27:44
 
 ### 문제 설명
 
--- 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
@@ -1,39 +1,36 @@
+import java.util.*;
 class Solution {
     public int[] solution(int n, int[] slicer, int[] num_list) {
-        int[] result;
+        List<Integer> result = new ArrayList<>();
         
         int a = slicer[0];
         int b = slicer[1];
         int c = slicer[2];
         
-        switch(n){
+        switch (n) {
             case 1:
-                result = new int[b + 1]; // 0번부터 b번까지
-                for(int i = 0; i <= b; i++){
-                    result[i] = num_list[i];
-                }               
-                break;
-            case 2:
-                result = new int[num_list.length - a]; // a번 인덱스부터 끝까지
-                for(int i = 0; i < num_list.length - a; i++){
-                    result[i] = num_list[a + i];
-                } 
-                break;
-            case 3:
-                result = new int[b - a + 1]; // a번 인덱스부터 b번 인덱스까지
-                for(int i = 0; i <= b - a; i++){
-                    result[i] = num_list[a + i];
+                for(int i=0; i <=b; i++){
+                    result.add(num_list[i]);
                 }
                 break;
-            default:
-                // c 간격으로 슬라이싱
-                int len = (b - a) / c + 1; // (b - a)간격으로 나누어 줄 수 있는 수
-                result = new int[len];
-                for(int i = 0, index = a; index <= b; i++, index += c){
-                    result[i] = num_list[index];
+             case 2:
+                for(int i=a; i <num_list.length; i++){
+                    result.add(num_list[i]);
                 }
                 break;
-        }
-        return result;
+             case 3:
+                for(int i=a; i <=b; i++){
+                    result.add(num_list[i]);
+                }
+                break;
+             case 4:
+                for(int i=a; i <=b; i+=c){
+                    result.add(num_list[i]);
+                }
+                break; 
+            default: break;
+        }                     
+                       
+        return result.stream().mapToInt(Integer::intValue).toArray();
     }
 }
