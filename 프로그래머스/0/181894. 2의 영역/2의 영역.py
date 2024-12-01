41eb84e689407e840fa463515295be8c4f1a6b19
RiaLeee <gyurim26@gmail.com>
2024-12-02 00:23:25 +0900

    [level 0] Title: 2의 영역, Time: 5.36 ms, Memory: 15.8 MB -BaekjoonHub

--- 프로그래머스/0/181894. 2의 영역/2의 영역.py
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.py
@@ -1,10 +1,17 @@
 def solution(arr):
-    # 2가 있는 인덱스를 저장할 리스트
-    positions = [i for i, num in enumerate(arr) if num == 2]
+    # 2가 있는 가장 작은 인덱스 찾기
+    start = -1
+    end = -1
     
-    # 2가 없다면 [-1]을 반환
-    if not positions:
+    for i in range(len(arr)):
+        if arr[i]==2:
+            if start == -1: #마지막 인덱스가 아니면
+                start =i
+            end =i
+    
+    #2가 하나도 없다면 -1 반환
+    if start == -1 :
         return [-1]
     
-    # 가장 작은 인덱스부터 가장 큰 인덱스까지 부분 배열을 반환
-    return arr[positions[0]:positions[-1] + 1]
+    return arr[start:end+1]
+                
