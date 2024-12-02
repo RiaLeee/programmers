5555d096ced81df640ec243c89872036c01d25df
RiaLeee <gyurim26@gmail.com>
2024-12-02 18:40:13 +0900

    [level 0] Title: n보다 커질 때까지 더하기, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.py
+++ 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.py
@@ -1,10 +1,7 @@
 def solution(numbers, n):
-    total_sum = 0  # 누적 합을 저장할 변수
+    sumNumbers = 0
     
-    # numbers 배열을 순회하며 누적 합을 구합니다.
-    for num in numbers:
-        total_sum += num  # 원소를 더합니다.
-        
-        # 만약 누적 합이 n보다 크면, 그 값을 바로 반환합니다.
-        if total_sum > n:
-            return total_sum
+    for i in range(len(numbers)):
+        sumNumbers += numbers[i]
+        if sumNumbers > n:
+            return sumNumbers
