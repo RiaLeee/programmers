1d15c301cde7ea41d9f29aea201dff14c344fc7c
RiaLeee <gyurim26@gmail.com>
2024-12-16 22:05:19 +0900

    [level 0] Title: 배열 비교하기, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.py
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.py
@@ -1,14 +1,16 @@
 def solution(arr1, arr2):
-    answer = 0
     
-    if len(arr1)!=len(arr2) :
-        answer=1 if len(arr1) > len(arr2) else -1
-    else : 
-        if sum(arr1) == sum(arr2) :
-            answer = 0
-        elif sum(arr1) > sum(arr2) :
-            answer = 1
-        else : answer = -1
+    sum1=sum(arr1)
+    sum2=sum(arr2)
         
-   
-    return answer
+    if len(arr1) < len(arr2):
+        return -1
+    elif len(arr1) > len(arr2):
+        return 1
+    else:
+        if sum1 < sum2:
+            return -1
+        elif sum1 > sum2:
+            return 1
+        else:
+            return 0
